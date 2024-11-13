2198b98e08f463ded436158ac9018cf7bff1cf5b
Ruthachere <ruthachere22@gmail.com>
2024-11-13 15:35:48 +0100

    defects from changing route name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('\my-project-env')
+@app.route('/my-project-env')
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
